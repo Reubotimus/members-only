00a7ebcdcd0e57796d7f67eb6bdc8ee37c00eda3
Reubotimus <reubencook555@gmail.com>
2024-08-14 20:49:43 +1000

    changed database connection to take environment variable

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,4 @@
 const {Pool} = require('pg');
 module.exports = new Pool({
-    host: "localhost", // or wherever the dbis hosted
-    user: "reubencook",
-    database: "members_only",
-    password: process.env.DATABASE_PASSWORD,
-    port: 5432 // The default port
+    connectionString: process.env.DATABASE_URL
 });
